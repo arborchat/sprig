cd48abe49e4d1d125f4c3ed44320f3214ab00e78
Jack Mordaunt <jackmordaunt@gmail.com>
2020-08-28 13:36:17 +0800

    chore: rm iconButton

--- reply-view.go
+++ reply-view.go
@@ -823,32 +823,3 @@ func (c *ReplyListView) layoutEditor(gtx layout.Context) layout.Dimensions {
 func (c *ReplyListView) SetManager(mgr ViewManager) {
 	c.manager = mgr
 }
-
-// iconButton applies defaults before rendering a `material.IconButtonStyle` to reduce noise.
-// The main paramaters for each button are the state and icon.
-// Color, size and inset are often the same.
-// This wrapper reduces noise by defaulting those things.
-type iconButton struct {
-	Theme  *theme.Theme
-	Button *widget.Clickable
-	Icon   *widget.Icon
-	Size   unit.Value
-	Inset  layout.Inset
-}
-
-func (btn iconButton) Layout(gtx C) D {
-	if btn.Size.V == 0 {
-		btn.Size = unit.Dp(buttonWidthDp)
-	}
-	if btn.Inset == (layout.Inset{}) {
-		btn.Inset = layout.UniformInset(unit.Dp(4))
-	}
-	return material.IconButtonStyle{
-		Background: btn.Theme.Color.Primary,
-		Color:      btn.Theme.Color.InvText,
-		Icon:       btn.Icon,
-		Size:       btn.Size,
-		Inset:      btn.Inset,
-		Button:     btn.Button,
-	}.Layout(gtx)
-}
